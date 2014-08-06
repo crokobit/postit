62b75ebd9606082d4c864f31ed5a6bcbe37b6a9e
crokobit <crokobit@gmail.com>
2014-08-06 23:34:17 +0800

    create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,13 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])  
   end
+
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.create(name: params[:category][:name]) 
+    render :new
+  end
 end
