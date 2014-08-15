a8a480c2eba23cb65615d702f77e7a9dbd6f2103
crokobit <crokobit@gmail.com>
2014-08-15 20:43:52 +0800

    capybara

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
+
+Capybara.javascript_driver = :webkit
+
+RSpec.configure do |config|
+end
